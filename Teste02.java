ff22ae04cfbb8da8a7f61e793ede98d31437a282
Eduardo Lengler <eduardolengler@gmail.com>
2024-04-25 11:56:13 -0300

    Exercício da Aula 06

---
+++ Teste02.java
@@ -0,0 +1,18 @@
+public class Teste02 {
+
+    public static void main(String[] args) {
+        String nome = "João";
+        imprimeNome ("Empty");
+    }
+
+    public static void imprimeNome(String nome) {
+        if(!nome.isEmpty()) {
+            System.out.println("Tudo bem "+ nome + "?");
+        } else {
+            System.out.println("O nome é " + nome + "?");
+        }
+
+        System.out.println(!nome.isEmpty());
+    }
+    
+}
